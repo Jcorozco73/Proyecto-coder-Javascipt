fbe50af492a65e3f4e5177de72f3334cab7b367a
jcorozco73 <jc.martin.orozco@gmail.com>
2023-03-11 12:15:39 -0300

    Primera entrega Proyecto

---
+++ codigo.js
@@ -0,0 +1,15 @@
+let edad = false
+let = validarCliente = function (time) {
+    edad = prompt( "Cual es tu edad?")
+    if (edad > 18) {
+      if (time >= 2 && time < 7 && free == false) {
+        alert(" Podes pasar, porque sos la primera persona despues de las 2 am")
+        free = true
+      } else {
+        alert(` son las ${time}:00 y podes pasar, pero tenes que pagar`)
+      } 
+  
+    } else {
+      alert("Sos menor de edad por ende no podes paasr")
+    }
+    }
