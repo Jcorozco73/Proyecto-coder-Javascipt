66e5855f0efbcd721ef39bc988f5c5c233c5680e
jcorozco73 <jc.martin.orozco@gmail.com>
2023-03-12 18:17:26 -0300

    Primer proyecto Coder

--- codigo.js
+++ codigo.js
@@ -1,15 +1,11 @@
-let edad = false
-let = validarCliente = function (time) {
-    edad = prompt( "Cual es tu edad?")
-    if (edad > 18) {
-      if (time >= 2 && time < 7 && free == false) {
-        alert(" Podes pasar, porque sos la primera persona despues de las 2 am")
-        free = true
-      } else {
-        alert(` son las ${time}:00 y podes pasar, pero tenes que pagar`)
-      } 
-  
-    } else {
-      alert("Sos menor de edad por ende no podes paasr")
-    }
-    }
+let tabla = parseInt(prompt(" Ingrese el numero de la tabla"))
+alert(`Empecemos con la tabla del ${tabla}`)
+
+for (let i = 0; i <= 10; i++) {
+       resultado = tabla * i
+
+       if (i=== 3) {
+        continue
+       }
+       alert(`${tabla}*${i}= ${resultado}`)
+}
