b34b71d0f379c468b8e0991411b93c2b486ce068
jcorozco73 <jc.martin.orozco@gmail.com>
2023-03-12 18:42:14 -0300

    Primer Proyecto tabla multiplicar

--- codigo.js
+++ codigo.js
@@ -1,11 +1,12 @@
-let tabla = parseInt(prompt(" Ingrese el numero de la tabla"))
+let tabla = parseInt(prompt(" ¿Que tabla quieres que calcule?"))
 alert(`Empecemos con la tabla del ${tabla}`)
 
 for (let i = 0; i <= 10; i++) {
        resultado = tabla * i
-
+        // Salta cuando i =3
        if (i=== 3) {
         continue
        }
+
        alert(`${tabla}*${i}= ${resultado}`)
 }
