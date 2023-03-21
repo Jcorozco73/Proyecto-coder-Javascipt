72f2c0884b2cfa8908e1a439a614c3b8fbc14587
jcorozco73 <jc.martin.orozco@gmail.com>
2023-03-21 17:32:24 -0300

    Implementado funciones

--- codigo.js
+++ codigo.js
@@ -1,12 +1,25 @@
 let tabla = parseInt(prompt(" ¿Que tabla quieres que calcule?"))
 alert(`Empecemos con la tabla del ${tabla}`)
 
-for (let i = 0; i <= 10; i++) {
-       resultado = tabla * i
-        // Salta cuando i =3
-       if (i=== 3) {
-        continue
-       }
+tablaMultiplicar(tabla)
 
-       alert(`${tabla}*${i}= ${resultado}`)
-}
+function tablaMultiplicar(tabla) {
+
+       for (let i = 0; i <= 10; i++) {
+              resultado = tabla * i
+               // Salta cuando i =3
+              if (i=== 3) {
+               alert("lo siento no se puede multiplicar los numeros por 3")      
+                     continue
+              } 
+              
+              alert(`${tabla}*${i}= ${resultado}`)
+              }
+              }
+   
+
+
+
+
+       
+       
